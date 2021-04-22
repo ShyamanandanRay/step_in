04501b833594b5c905948068d2ddcdf4447736d1
shyama <shyamanandan.ray7381@gmail.com>
2021-04-22 17:40:04 +0530

    question 1 "write a program to check user input abbreviation"

---
+++ question 1.py
@@ -0,0 +1,14 @@
+# write a program to check user input abbreviation
+print("Enter the abbreviation\n")
+abr = input()
+if abr == "lol":
+    print("laughing out loud")
+elif abr == "rofl":
+    print("rolling on floor laughing")
+elif abr == "lmk":
+    print("Let me know")
+elif abr == "smh":
+    print("shaking my head")
+else:
+    print("Not valid abbreviation")
+
